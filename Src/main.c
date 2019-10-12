fbd443a1f4818fbdb75b9c44e00d7b97db1958e7
Adam Hrastnik <adam.hrastnik@gmail.com>
2019-10-12 14:26:24 +0200

    Created a simple blinky example; added the debug config.

--- Src/main.c
+++ Src/main.c
@@ -99,6 +99,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
